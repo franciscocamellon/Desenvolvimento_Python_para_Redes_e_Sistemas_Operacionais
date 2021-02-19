281b2cf896823dd8228b67e48eb3d1935c19ab0a
franciscocamellon <franciscocamellon@gmail.com>
2021-02-19 20:57:31 -0300

    Finished question 3

--- TP01/questao03.py
+++ TP01/questao03.py
@@ -11,6 +11,7 @@
 """
 
 import os
+import sys
 
 
 class Questao_03():
@@ -20,20 +21,27 @@ class Questao_03():
 
     def __init__(self):
         """ Constructor """
+        self.platform = sys.platform
+        self.os_name = os.name
+        self.pid = ''
 
     def init_class(self):
         """ This function receives the input data from users. """
-        
+        pass
 
     def process_data(self):
         """ This function process the input data from init_class. """
-       
+        if self.platform.startswith('linux'):
+            self.pid = os.getgid()
+        elif self.platform.startswith('win32'):
+            self.pid = os.getpid()
 
     def print_result(self):
         """ This is a printer! It prints. """
-        
+        self.process_data()
         print('===' * 25, 'Questão 03'.center(75), '===' * 25, sep='\n')
-        print('Nome do usuário logado: {}'.format(),
+        print('Sistema operacional: {}'.format('Windows' if self.platform == 'win32' else 'Linux'),
+              'PID deste processo: {}'.format(self.pid),
               '---' * 25, 'Aluno: Francisco Camello'.rjust(75), sep="\n")
 
 
