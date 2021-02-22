a1b1471f2064b52245f4e4ae730b3b3c01f2a095
franciscocamellon <franciscocamellon@gmail.com>
2021-02-22 19:13:17 -0300

    Finish question 19

--- TP01/questao19.py
+++ TP01/questao19.py
@@ -11,6 +11,8 @@
 """
 
 import os
+import psutil
+from psutil._common import bytes2human
 
 
 class Questao_19():
@@ -20,21 +22,31 @@ class Questao_19():
 
     def __init__(self):
         """ Constructor """
+        self.error = ''
+        print('===' * 25, 'Questão 19'.center(75), '===' * 25, sep='\n')
+        self.init_class()
+        self.process_data()
 
     def init_class(self):
         """ This function receives the input data from users. """
-       
+        pass
 
     def process_data(self):
         """ This function process the input data from init_class. """
-        
+        self.partitions = psutil.disk_partitions()
+        self.usage = psutil.disk_usage(self.partitions[0][0])
 
     def print_result(self):
         """ This is a printer! It prints. """
-        
-        print('===' * 25, 'Questão 19'.center(75), '===' * 25, sep='\n')
-        print('Nome do usuário logado: {}'.format(),
-              '---' * 25, 'Aluno: Francisco Camello'.rjust(75), sep="\n")
+
+        if self.error:
+            print(self.error,
+                  '---' * 25, 'Aluno: Francisco Camello'.rjust(75), sep='\n')
+        else:
+            print('Partição do sistema {}\n\tArmazenamento disponível: {}'.format(
+                self.partitions[0][0],
+                bytes2human(self.usage[2])))
+            print('---' * 25, 'Aluno: Francisco Camello'.rjust(75), sep='\n')
 
 
 Questao_19().print_result()
