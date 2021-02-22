d2ca0009a2ea9610118822313b5c6dcb97eb0b91
franciscocamellon <franciscocamellon@gmail.com>
2021-02-22 14:47:52 -0300

    Finished question 13

--- TP01/questao13.py
+++ TP01/questao13.py
@@ -11,29 +11,41 @@
 """
 
 import os
+import sys
+import subprocess
 
 
 class Questao_13():
     """
-    This program gets the logged username.
+    This program creates a process with subprocess module and prints your PID.
     """
 
     def __init__(self):
         """ Constructor """
+        self.platform = sys.platform
+        self.os_name = os.name
+        self.pid = ''
+        print('===' * 25, 'Questão 13'.center(75), '===' * 25, sep='\n')
+        self.init_class()
+        self.process_data()
 
     def init_class(self):
         """ This function receives the input data from users. """
-        
+        pass
 
     def process_data(self):
         """ This function process the input data from init_class. """
-        
+        if self.platform.startswith('linux'):
+            self.pid = os.getgid()
+        elif self.platform.startswith('win32'):
+            process = subprocess.Popen("notepad")
+            self.pid = process.pid
 
     def print_result(self):
         """ This is a printer! It prints. """
-        
-        print('===' * 25, 'Questão 13'.center(75), '===' * 25, sep='\n')
-        print('Nome do usuário logado: {}'.format(e),
+
+        print('Sistema operacional: {}'.format('Windows' if self.platform == 'win32' else 'Linux'),
+              'PID deste processo: {}'.format(self.pid),
               '---' * 25, 'Aluno: Francisco Camello'.rjust(75), sep="\n")
 
 
