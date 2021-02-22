c32d26d55708fce44b04085b2b812cb80c8ef752
franciscocamellon <franciscocamellon@gmail.com>
2021-02-22 19:12:28 -0300

    Finish question 16

--- TP01/questao16.py
+++ TP01/questao16.py
@@ -11,6 +11,8 @@
 """
 
 import os
+import psutil
+import time
 
 
 class Questao_16():
@@ -20,21 +22,37 @@ class Questao_16():
 
     def __init__(self):
         """ Constructor """
+        self.error = ''
+        self.times_cpu = list()
+        print('===' * 25, 'Questão 16'.center(75), '===' * 25, sep='\n')
+        self.init_class()
+        self.process_data()
 
     def init_class(self):
         """ This function receives the input data from users. """
+        pass
         
 
     def process_data(self):
         """ This function process the input data from init_class. """
-        
+        self.times_cpu = psutil.cpu_times_percent(interval=0.1, percpu=True)
 
     def print_result(self):
         """ This is a printer! It prints. """
         
-        print('===' * 25, 'Questão 16'.center(75), '===' * 25, sep='\n')
-        print('Nome do usuário logado: {}'.format(),
-              '---' * 25, 'Aluno: Francisco Camello'.rjust(75), sep="\n")
+        if self.error:
+            print(self.error,
+                  '---' * 25, 'Aluno: Francisco Camello'.rjust(75), sep='\n')
+        else:
+            print('Número de núcleos: {}\n'.format(len(self.times_cpu)))
+            for core in range(len(self.times_cpu)):
+                print('{}Núcleo {}: user={} system={} idle={}'.format(
+                    ' '*3,
+                    core,
+                    self.times_cpu[core][0],
+                    self.times_cpu[core][1],
+                    self.times_cpu[core][2]))
+            print('---' * 25, 'Aluno: Francisco Camello'.rjust(75), sep='\n')
 
 
 Questao_16().print_result()
