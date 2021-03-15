dd80f8f3cca6602d5135ed271169d84c08cf3867
franciscocamellon <franciscocamellon@gmail.com>
2021-03-15 13:10:25 -0300

    Finished question 10

--- TP03/questao10.py
+++ TP03/questao10.py
@@ -12,30 +12,57 @@
 
 
 import socket
+import sys
+import os
+
 
 class Questao_10():
-    """ This is a TCP server program. """
+    """ This is a TCP client program. """
 
     def __init__(self):
         """ Constructor """
+        os.chdir(os.path.dirname(__file__))
+        self.cwd = os.getcwd()
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client_host = socket.gethostname()
         self.door = 8881
-        # self.server_socket.bind((self.server_host, self.door))
-        # self.server_socket.listen()
-        # print('Servidor de nome {} esperando conexão na porta {}'.format(self.server_host, self.door))
+        self.file_name = input('Entre com o nome do arquivo: ')
 
         try:
-            # Tenta se conectar ao servidor
             self.client_socket.connect((self.client_host, self.door))
-            msg = "Ola servidor!\n"
-            # Envia mensagem codificada em bytes ao servidor
-            self.client_socket.send(msg.encode('ascii'))
-            # Fecha conexão com o servidor
-            self.client_socket.close()
-        except Exception as erro:
-            print(str(erro))
+            self.client_socket.send(self.file_name.encode('ascii'))
+            msg = self.client_socket.recv(12)
+            file_size = int(msg.decode('ascii'))
+            if file_size >= 0:
+                dowload_dir = self.cwd + '\\download\\'
+                if not os.path.exists(dowload_dir):
+                    os.mkdir(dowload_dir)
+                _file = open(dowload_dir + self.file_name, 'wb')
+                count = 0
+                _bytes = self.client_socket.recv(4096)
 
+                while _bytes:
+                    _file.write(_bytes)
+                    count = count + len(_bytes)
+                    os.system('cls')
+                    self.download_status(count, file_size)
+                    _bytes = self.client_socket.recv(4096)
+                _file.close()
+            else:
+                print('Arquivo não encontrado no servidor!')
+        except Exception as err:
+            print(str(err))
+            self.client_socket.close()
+        input("Pressione qualquer tecla para sair...")
+
+    def download_status(self, _bytes, _size):
+        """ Docstring """
+        kbytes = _bytes/1024
+        tam_bytes = _size/1024
+        texto = 'Baixando... '
+        texto = texto + '{:<.2f}'.format(kbytes) + ' KB '
+        texto = texto + 'de ' + '{:<.2f}'.format(tam_bytes) + ' KB'
+        print(texto)
 
 
 Questao_10()
