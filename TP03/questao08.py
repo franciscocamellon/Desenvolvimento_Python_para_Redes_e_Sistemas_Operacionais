9451cbe4f373b2467072f12d7d828bc33d698ca4
franciscocamellon <franciscocamellon@gmail.com>
2021-03-15 16:28:19 -0300

    Initial code

--- TP03/questao08.py
+++ TP03/questao08.py
@@ -9,8 +9,47 @@
 *        Nome do arquivo : questao_08.py                                   *
 ***************************************************************************/
 """
+import socket
+import time
+import pickle
 
-class Questao_08():
-    """ This is a cliente program. """
 
+class Questao_10():
+    """ This is a UDP client program. """
+
+    def __init__(self):
+        """ Constructor """
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.client_host = socket.gethostname()
+        self.door = 9991
+        
+        try:
+            self.client_socket.connect((self.client_host, self.door))
+            msg = ''
+            print('{:>8}'.format('%CPU')+'{:>8}'.format('%MEM'))
+            for i in range(10):
+                # Envia mensagem vazia apenas para indicar a requisição
+                s.send(msg.encode('ascii'))
+                bytes = s.recv(1024)
+                # Converte os bytes para lista
+                lista = pickle.loads(bytes)
+                self.imprime(lista)
+                time.sleep(2)
+            msg = 'fim'
+            s.send(msg.encode('ascii'))
+        except Exception as erro:
+            print(str(erro))
+
+        # Fecha o socket
+        s.close()
+
+        input("Pressione qualquer tecla para sair...")
     
+    def imprime(self, l):
+      texto = ''
+      for i in l:
+         texto = texto + '{:>8.2f}'.format(i)
+      print(texto) 
+
+
+Questao_10()
--- TP03/questao09.py
+++ TP03/questao09.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-9 -*-
+# -*- coding: utf-8 -*-
 """
 /************************ TESTE DE PERFORMANCE 03 **************************
 *    Questao 09                                                            *
@@ -9,3 +9,45 @@
 *        Nome do arquivo : questao_09.py                                   *
 ***************************************************************************/
 """
+import socket
+import psutil
+import pickle
+
+
+class Questao_09():
+    """ This is a UDP server program. """
+
+    def __init__(self):
+        """ Constructor """
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.server_host = socket.gethostname()
+        self.door = 9991
+        self.server_socket.bind((self.server_host, self.door))
+        self.server_socket.listen()
+        print('Servidor de nome {} esperando conexão na porta {}'.format(self.server_host, self.door))
+
+        (socket_cliente, addr) = self.server_socket.accept()
+        print("Conectado a:", str(addr))
+
+        while True:
+            # Recebe pedido do cliente:
+            msg = socket_cliente.recv(4)
+            if msg.decode('ascii') == 'fim':
+                break
+            # Gera a lista de resposta
+            resposta = []
+            resposta.append(psutil.cpu_percent())
+            mem = psutil.virtual_memory()
+            mem_percent = mem.used/mem.total
+            resposta.append(mem_percent)
+            # Prepara a lista para o envio
+            bytes_resp = pickle.dumps(resposta)
+            # Envia os dados
+            socket_cliente.send(bytes_resp)
+
+        # Fecha socket do servidor e cliente
+        socket_cliente.close()
+        self.server_socket.close()
+
+
+Questao_09()
