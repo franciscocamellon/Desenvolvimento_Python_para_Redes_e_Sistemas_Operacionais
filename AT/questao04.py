9c09838297f830b201e3623645c290227d753a28
franciscocamellon <franciscocamellon@gmail.com>
2021-03-21 15:44:03 -0300

    Finished

--- AT/questao04.py
+++ AT/questao04.py
@@ -12,6 +12,8 @@
 import psutil
 import time
 from psutil._common import bytes2human
+
+
 class Questao_04():
     """
     This program gets the logged username.
@@ -19,8 +21,9 @@ class Questao_04():
 
     def __init__(self):
         """ Constructor """
-        self.info_dict = {}
-        self.username = ''
+        self.line_file_list = list()
+        print('===' * 25, 'Questão 04'.center(75), '===' * 25, sep='\n')
+        self.init_class()
         self.process_data()
 
     def init_class(self):
@@ -30,28 +33,18 @@ class Questao_04():
     def process_data(self):
         """ This function process the input data from init_class. """
 
-        for i in psutil.process_iter():
-            if psutil.pid_exists(i.pid):
-                self.info_dict[i.pid] = [
-                    i.name(),
-                    i.cpu_percent(),
-                    round(i.memory_percent(), 2)
-                ]
-            else:
-                pass
-        return self.info_dict
+        with open('arquivo.txt', 'r', encoding='utf-8', newline='\r\n') as _file:
+            for line in _file:
+                res = line[::-1].strip()
+                self.line_file_list.append(res)
+        self.line_file_list.sort(reverse=True)
+        # print(self.line_file_list)
+        return self.line_file_list
 
     def print_result(self):
         """ This is a printer! It prints. """
-        # info_dict = self.process_data()
-        print('===' * 25, 'Questão 04'.center(75), '===' * 25, sep='\n')
-        print('{}{:<7}{:<10}{:<10}{:<10}'.format(' '*2, 'PID',
-                                                 'CPU %', 'MEM %', 'Nome'), '---' * 25, sep='\n')
-        time.sleep(1)
-        for pid, info_pid in self.info_dict.items():
-            print('{}{:<7}{:<10}{:<10}{:<10}'.format(
-                ' '*2, pid, info_pid[1], info_pid[2], info_pid[0]))
-            time.sleep(0.1)
+        for item in self.line_file_list:
+            print(item)
         print('---' * 25, 'Aluno: Francisco Camello'.rjust(75), sep="\n")
 
 
