8a59db340529c1466990e8190aece6cdc4f3b7bd
franciscocamellon <franciscocamellon@gmail.com>
2021-03-21 20:09:43 -0300

    Finished

--- AT/questao05.py
+++ AT/questao05.py
@@ -12,6 +12,8 @@
 import psutil
 import time
 from psutil._common import bytes2human
+
+
 class Questao_05():
     """
     This program gets the logged username.
@@ -20,38 +22,53 @@ class Questao_05():
     def __init__(self):
         """ Constructor """
         self.info_dict = {}
-        self.username = ''
+        self.a_list = list()
+        self.b_list = list()
+        self.list_sum = list()
+        self.init_class()
         self.process_data()
 
     def init_class(self):
         """ This function receives the input data from users. """
-        pass
+        with open('a.txt', 'r', encoding='utf-8') as a_file:
+            for item in a_file.readlines():
+                a_list = item.split(' ')
+        a_file.close()
+        for _int in a_list:
+            self.a_list.append(int(_int))
+
+        with open('b.txt', 'r', encoding='utf-8') as b_file:
+            for item in b_file.readlines():
+                b_list = item.split(' ')
+        b_file.close()
+        for _int in b_list:
+            self.b_list.append(int(_int))
 
     def process_data(self):
         """ This function process the input data from init_class. """
 
-        for i in psutil.process_iter():
-            if psutil.pid_exists(i.pid):
-                self.info_dict[i.pid] = [
-                    i.name(),
-                    i.cpu_percent(),
-                    round(i.memory_percent(), 2)
-                ]
-            else:
-                pass
-        return self.info_dict
+        len_a = len(self.a_list)
+        len_b = len(self.b_list)
+        dif = len_a - len_b
+
+        if dif > 0:
+            for i in range(0, abs(dif)):
+                self.b_list.append(0)
+        elif dif < 0:
+            for i in range(0, abs(dif)):
+                self.a_list.append(0)
+        else:
+            pass
+        for a, b in zip(self.a_list, self.b_list):
+            self.list_sum.append(sum([a, b]))
 
     def print_result(self):
         """ This is a printer! It prints. """
-        # info_dict = self.process_data()
-        print('===' * 25, 'Questão 05'.center(75), '===' * 25, sep='\n')
-        print('{}{:<7}{:<10}{:<10}{:<10}'.format(' '*2, 'PID',
-                                                 'CPU %', 'MEM %', 'Nome'), '---' * 25, sep='\n')
-        time.sleep(1)
-        for pid, info_pid in self.info_dict.items():
-            print('{}{:<7}{:<10}{:<10}{:<10}'.format(
-                ' '*2, pid, info_pid[1], info_pid[2], info_pid[0]))
-            time.sleep(0.1)
+        print('===' * 25, 'Questão 05'.center(75), '===' * 25,
+              'A soma dos números nos arquivos a.txt e b.txt é: ', sep='\n')
+        for _sum in self.list_sum:
+            print('{}{:<7}'.format(' '*2, _sum))
+            time.sleep(0.5)
         print('---' * 25, 'Aluno: Francisco Camello'.rjust(75), sep="\n")
 
 
