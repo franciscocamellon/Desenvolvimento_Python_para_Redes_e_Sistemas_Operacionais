a4c13ed5819c346966e9de381975424b01f28df4
franciscocamellon <franciscocamellon@gmail.com>
2021-03-21 11:44:45 -0300

    Finished

--- AT/questao02.py
+++ AT/questao02.py
@@ -9,3 +9,54 @@
 *        Nome do arquivo : questao_02.py                                   *
 ***************************************************************************/
 """
+import os
+import errno
+
+class Questao_02():
+    """
+    This program creates a process and opens a user-given text file.
+    """
+
+    def __init__(self):
+        """ Constructor """
+        self.search_path, self.error = '', ''
+        self.file_name = 'text_file.txt'
+        self.file_path = '.\\7'  # dado inexistente forçando input
+        print('===' * 25, 'Questão 11'.center(75), '===' * 25, sep='\n')
+        self.init_class()
+        self.process_data()
+
+    def init_class(self):
+        """ This function receives the input data from users. """
+
+        self.search_path = os.path.join(self.file_path, self.file_name)
+        if os.path.exists(self.search_path):
+            pass
+        else:
+            self.file_path = input(str('Digite o caminho de um arquivo: '))
+            os.chdir(self.file_path)
+            self.file_name = input('Digite o nome do arquivo: ')
+            self.search_path = os.path.join(self.file_path, self.file_name)
+
+    def process_data(self):
+        """ This function process the input data from init_class. """
+        if os.path.isfile(self.search_path):
+            os.startfile(self.search_path)
+        elif os.path.isdir(self.search_path):
+            self.error = 'É um diretório!'
+        else:
+            self.error = FileNotFoundError(errno.ENOENT, os.strerror(
+                errno.ENOENT), os.path.basename(self.search_path))
+
+    def print_result(self):
+        """ This is a printer! It prints. """
+        if self.error:
+            print(self.error,
+                  '---' * 25, 'Aluno: Francisco Camello'.rjust(75), sep="\n")
+        else:
+            print('\nO arquivo {} foi aberto!'.format(self.file_name),
+                  '---' * 25, 'Aluno: Francisco Camello'.rjust(75), sep="\n")
+
+
+
+Questao_02().print_result()
