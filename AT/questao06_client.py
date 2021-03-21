2475c8a6a93182841835d7a173adad13f276ddc0
franciscocamellon <franciscocamellon@gmail.com>
2021-03-21 20:16:51 -0300

    Initial code

---
+++ AT/questao06_client.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+/****************************** ASSESSMENT *********************************
+*    Questao 06                                                            *
+*        Aluno           : Francisco Alves Camello Neto                    *
+*        Disciplina      : Desenvolvimento Python para Redes e             *
+*                           Sistemas Operacionais                          *
+*        Professor       : Thaís do Nascimento Viana                       *
+*        Nome do arquivo : questao_06.py                                   *
+***************************************************************************/
+"""
+import socket
+import sys
+import os
+
+class Questao_06():
+    """ This is a TCP client program. """
+
+    def __init__(self):
+        """ Constructor """
+        os.chdir(os.path.dirname(__file__))
+        self.cwd = os.getcwd()
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_host = socket.gethostname()
+        self.door = 8881
+        self.file_name = input('Entre com o nome do arquivo: ')
+
+        try:
+            self.client_socket.connect((self.client_host, self.door))
+            self.client_socket.send(self.file_name.encode('ascii'))
+            msg = self.client_socket.recv(12)
+            file_size = int(msg.decode('ascii'))
+            if file_size >= 0:
+                dowload_dir = self.cwd + '\\download\\'
+                if not os.path.exists(dowload_dir):
+                    os.mkdir(dowload_dir)
+                _file = open(dowload_dir + self.file_name, 'wb')
+                count = 0
+                _bytes = self.client_socket.recv(4096)
+
+                while _bytes:
+                    _file.write(_bytes)
+                    count = count + len(_bytes)
+                    os.system('cls')
+                    self.download_status(count, file_size)
+                    _bytes = self.client_socket.recv(4096)
+                _file.close()
+            else:
+                print('Arquivo não encontrado no servidor!')
+        except Exception as err:
+            print(str(err))
+            self.client_socket.close()
+        input("Pressione qualquer tecla para sair...")
+
+    def download_status(self, _bytes, _size):
+        """ Docstring """
+        kbytes = _bytes/1024
+        tam_bytes = _size/1024
+        texto = 'Baixando... '
+        texto = texto + '{:<.2f}'.format(kbytes) + ' KB '
+        texto = texto + 'de ' + '{:<.2f}'.format(tam_bytes) + ' KB'
+        print(texto)
+
+Questao_06().print_result()
---
+++ AT/questao06_server.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+/****************************** ASSESSMENT *********************************
+*    Questao 06                                                            *
+*        Aluno           : Francisco Alves Camello Neto                    *
+*        Disciplina      : Desenvolvimento Python para Redes e             *
+*                           Sistemas Operacionais                          *
+*        Professor       : Thaís do Nascimento Viana                       *
+*        Nome do arquivo : questao_06.py                                   *
+***************************************************************************/
+"""
+import socket
+import os
+
+class Questao_06():
+    """ This is a TCP server program. """
+
+    def __init__(self):
+        """ Constructor """
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_host = socket.gethostname()
+        self.door = 8881
+        self.server_socket.bind((self.server_host, self.door))
+        self.server_socket.listen()
+        print('\nServidor de nome {} esperando conexão na porta {}'.format(
+            self.server_host, self.door))
+
+        while True:
+            (socket_cliente, addr) = self.server_socket.accept()
+            print("Conectado a:", str(addr))
+            msg = socket_cliente.recv(2048)
+            file_name = msg.decode('ascii')
+            if os.path.isfile(file_name):
+                file_size = os.stat(file_name).st_size
+                socket_cliente.send(str(file_size).encode('ascii'))
+                _file = open(file_name, 'rb')
+                _bytes = _file.read(4096)
+                while _bytes:
+                    socket_cliente.send(_bytes)
+                    _bytes = _file.read(4096)
+                _file.close()
+            else:
+                print('Arquivo não encontrado!')
+                socket_cliente.send('-1'.encode('ascii'))
+            socket_cliente.close()
+
+
+Questao_06()
